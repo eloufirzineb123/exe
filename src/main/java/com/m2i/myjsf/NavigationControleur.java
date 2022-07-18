4e0d4a8e40622a0e178f0e62538846e7c3ed0b62
eloufir <eloufir123@gmail.com>
2022-07-18 14:30:27 +0200

    navigation conditio

--- src/main/java/com/m2i/myjsf/NavigationControleur.java
+++ src/main/java/com/m2i/myjsf/NavigationControleur.java
@@ -5,11 +5,23 @@
 package com.m2i.myjsf;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 
 @ManagedBean(name = "navigationControleur")
 //@Named(value = "navigationControleur")
 //@Dependent
 public class NavigationControleur {
+    
+    @ManagedProperty (value = "#{param.pageId}")
+    private String pageId;
+
+    public void setPageId(String pageId) {
+        this.pageId = pageId;
+    }
+
+    public String getPageId() {
+        return pageId;
+    }
 
     
     public NavigationControleur() {
@@ -17,6 +29,13 @@ public class NavigationControleur {
     
     public String moveToHelloPage(){
         return "hello";
+    }
+    
+    public String goToPage(){
+        if(pageId !=null){
+         return pageId;   
+        }
+        return "home";
     }
     
 }
